5eae85bf18f11ca4242dc0173d6e09890f534949
Jonathan D. Dominguez <jonathan.d.dominguez@hotmail.com>
2017-10-20 15:11:51 -0300

    Arregle el problema de la indentación.

--- src/Parse.py
+++ src/Parse.py
@@ -1,24 +1,5 @@
-try:
-	from bs4 import BeautifulSoup
-	import request
-except ImportError as e:
-	raise ("Faltan instalar librerias: \n {0}".format(e) )
-else:
-	pass
-
-
-class ListaCapitulos(list):
-	lista = []
-	"""Clase donde se listan todos los capitulos"""
-	def __init__(self, arg):
-		super().__init__()
-		self.arg = arg 
-
-class Lista(dict):
-	"""Clase donde se agrupa la lista de capitulos 
-	   con su correspondiente numero de capitulo."""
-	def function(self):
-		pass
+from bs4 import BeautifulSoup
+import request
 		
 		
 
@@ -42,35 +23,32 @@ class Jkanime(Conexion):
 		self.urlbase = 'http://jkanime.net'
 
 	def link(self):
-        #req = requests.get(self.url)
-        #self.soup = bs(req.text, "html.Parser")
-        self.soup = self._conn()
-        src = self.soup.find('iframe', {'class': 'player_conte'})[
-            'src'].replace('jk.php?u=', '')
-        return str(src)
+		self.soup = self._conn()
+		src = self.soup.find('iframe', {'class': 'player_conte'})['src'].replace('jk.php?u=','')
+		return str(src)
+    
+	def nombre(self):
+		nombre = self.soup.find('div', {'class': 'vervideo'})
+		return nombre.string
+	
+	def programacion(self):
+		items = self.soup.find('li', {'class': 'ratedul'})
+		anime_Name = []
+		anime_Url = {}
+		for item in items:
+			name = item.find_all('a')[1].get_text()
+			url = item.find_all('a')[1]['href']
+			animeNames.append(name)
+			animeUrls.update({name: url})
+		return anime_Url, anime_Name
+		print(anime_Url)
 
-    def nombre(self):
-        nombre = self.soup.find('div', {'class': 'vervideo'})
-        return nombre.string
+	def ver_programacion(self):
 
-    def programacion(self):
-        items = self.soup.find('li', {'class': 'ratedul'})
-        anime_Name = []
-        anime_Url = {}
-        for item in items:
-            name = item.find_all('a')[1].get_text()
-            url = item.find_all('a')[1]['href']
-            animeNames.append(name)
-            animeUrls.update({name: url})
-        return anime_Url, anime_Name
-        print(anime_Url)
-
-    def ver_programacion(self):
-
-    	for item in items:
-    		items = self.soup.find('ul', {'class': 'ratedul'})
-    		name2 = items.find_all('a').get_text()
-    		print(name2)
+		for item in items:
+			items = self.soup.find('ul', {'class': 'ratedul'})
+			name2 = items.find_all('a').get_text()
+			print(name2)
 
 	def Capitulo(self):
 		pass
